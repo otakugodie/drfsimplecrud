041ec4e750e42cdd122ef9858b8018bbc177b368
otakugodie <95259091+otakugodie@users.noreply.github.com>
2023-04-25 15:01:41 +0100

    Adjust build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,11 @@
 set -o errexit
 
 pip install -r requirements.txt
-<<<<<<< HEAD
 
 if [[ $CREATE_SUPERUSER ]];
 then
   python drfsimplecrud-42q0/manage.py createsuperuser --no-input
 fi
-=======
->>>>>>> 75036a470fdcce7930655e907bbd663ee1a810b4
 
 python manage.py collectstatic --no-input
 python manage.py migrate
