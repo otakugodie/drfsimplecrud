c02a1b0c923c3a2e671bd7706a55c1a8d827f82e
otakugodie <95259091+otakugodie@users.noreply.github.com>
2023-04-25 14:59:22 +0100

    Add createuser

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python drfsimplecrud-42q0/manage.py createsuperuser --no-input
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
